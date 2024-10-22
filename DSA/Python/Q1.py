ea872c301fc49eb288399caf7567ea3516cc3573
Vaibhav08-eng <khannavaibhav05@gmail.com>
2024-10-22 21:17:49 +0530

    Update Q1.py

--- DSA/Python/Q1.py
+++ DSA/Python/Q1.py
@@ -1,5 +1,12 @@
 def generate_pascal_triangle(n):
     #write your code here
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    print(triangle)
 
 if __name__ == "__main__":
     n = 5
