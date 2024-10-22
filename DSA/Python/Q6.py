f4277ce0fe7ad8bf94b60fddff303f039c50c8d7
Vaibhav08-eng <khannavaibhav05@gmail.com>
2024-10-22 21:25:59 +0530

    Update Q6.py

--- DSA/Python/Q6.py
+++ DSA/Python/Q6.py
@@ -1,7 +1,18 @@
 # Driver Code
 def number_spiral(y, x):
     #write yout code here
-
+    n = max(abs(x), abs(y))
+    
+    largest_number = (2 * n) ** 2
+    
+    if x == n: 
+        return largest_number - (n - y)
+    elif y == -n: 
+        return largest_number - (2 * n) + (n - x)
+    elif x == -n: 
+        return largest_number - (3 * n) + (y + n)
+    else:  
+        return largest_number - (n - x)
 # Default Test Cases
 test_cases = [
     (2, 3),  # Output: 8
